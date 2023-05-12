209995afa5eb5810dbd2091d3c75cf756928a2ac
nickr <nicholaspriegel@gmail.com>
2023-05-12 15:24:31 -0400

    .

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Memory Game
+# The Egyptian Deities
 
-This is a simple memory game created with React. 
+I wanted to build something to help me learn about and remember the Egyptian Deities
 
-Images my take some time to download.
+I found these wonderful images on at https://symbolsage.com/egyptian-gods-list/ and had to build something with them!
 
 Project may be viewed on github pages: https://nicholas-riegel.github.io/memory_game_react/
 
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,19 +3,19 @@ import React from 'react'
 export default function Sidebar({shuffle, godlist}) {
     return (
         <div className='sidebar'>
-			<h1>Learn some of the main Egyptian deities</h1>
+			<h1>Learn about the Egyptian deities!</h1>
             <br />
-			<h3>Click on an image to learn about the deity</h3>
+			<h3 style={{ marginTop: '2rem' }}>Click an image to test your memory and learn more!</h3>
             <br />
         
-            <button onClick={()=>shuffle(godlist)}>Shuffle</button>
+            <button onClick={()=>shuffle(godlist)} style={{ width: '4rem', marginTop: '1rem', marginBottom: '2rem'}}>Shuffle</button>
             <br />
             <br />
             {godlist.find(x=>x.clicked===true)
                 ?   godlist.map(x=>x.clicked===true && <div><h3>{x.name}</h3><p>{x.description}</p></div>)
                 :   <div>
                         <h3>Acknowledgements</h3>
-                        <p>Images are from the wonderful website, <a href="https://symbolsage.com/egyptian-gods-list/">SymbolSage</a>. Information about the gods is from <a href="https://symbolsage.com/egyptian-gods-list/">SymbolSage</a>, <a href="https://www.britannica.com/list/11-egyptian-gods-and-goddesses">Britannica</a>, and <a href="https://www.worldhistory.org/article/885/egyptian-gods---the-complete-list/">WorldHistory</a>.</p>
+                        <p>Images: <a href="https://symbolsage.com/egyptian-gods-list/">SymbolSage</a>.</p> <p>Information: <a href="https://symbolsage.com/egyptian-gods-list/">SymbolSage</a>, <a href="https://www.britannica.com/list/11-egyptian-gods-and-goddesses">Britannica</a>, <a href="https://www.worldhistory.org/article/885/egyptian-gods---the-complete-list/">WorldHistory</a>.</p>
                     </div>
             }
         </div>
